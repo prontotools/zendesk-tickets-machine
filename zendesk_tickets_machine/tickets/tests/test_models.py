62aa26a8a2472723e17d8d2baaab47f58905fee2
zkan <kan@prontomarketing.com>
2016-12-10 20:11:57 +0700

    Add 6 more fields
    - group
    - status
    - private_comment
    - zendesk_ticket_id
    - stage
    - vertical

--- zendesk_tickets_machine/tickets/tests/test_models.py
+++ zendesk_tickets_machine/tickets/tests/test_models.py
@@ -16,9 +16,15 @@ class TicketTest(TestCase):
         ticket.requester_id = '1095195473'
         ticket.assignee = 'kan@prontomarketing.com'
         ticket.assignee_id = '1095195243'
+        ticket.group = 'Marketing Services'
         ticket.ticket_type = 'question'
         ticket.priority = 'urgent'
         ticket.tags = 'welcome'
+        ticket.status = 'open'
+        ticket.private_comment = 'Private comment'
+        ticket.zendesk_ticket_id = '24328'
+        ticket.stage = 'A'
+        ticket.vertical = 'NASS'
         ticket.save()
 
         ticket = Ticket.objects.last()
@@ -29,6 +35,12 @@ class TicketTest(TestCase):
         self.assertEqual(ticket.requester_id, '1095195473')
         self.assertEqual(ticket.assignee, 'kan@prontomarketing.com')
         self.assertEqual(ticket.assignee_id, '1095195243')
+        self.assertEqual(ticket.group, 'Marketing Services')
         self.assertEqual(ticket.ticket_type, 'question')
         self.assertEqual(ticket.priority, 'urgent')
         self.assertEqual(ticket.tags, 'welcome')
+        self.assertEqual(ticket.status, 'open')
+        self.assertEqual(ticket.private_comment, 'Private comment')
+        self.assertEqual(ticket.zendesk_ticket_id, '24328')
+        self.assertEqual(ticket.stage, 'A')
+        self.assertEqual(ticket.vertical, 'NASS')
