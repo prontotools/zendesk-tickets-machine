e73016c5c7c43d08e1dd28cd389629f23545bc6f
zkan <kan@prontomarketing.com>
2016-12-10 09:33:51 +0700

    Add requester_id and assignee_id fields

--- zendesk_tickets_machine/tickets/models.py
+++ zendesk_tickets_machine/tickets/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Ticket(models.Model):
     subject = models.CharField(max_length=300)
     requester = models.CharField(max_length=100)
+    requester_id = models.CharField(max_length=50)
     assignee = models.CharField(max_length=100)
+    assignee_id = models.CharField(max_length=50)
     ticket_type = models.CharField(max_length=50)
     priority = models.CharField(max_length=50)
     tags = models.CharField(max_length=300)
--- zendesk_tickets_machine/tickets/tests/test_models.py
+++ zendesk_tickets_machine/tickets/tests/test_models.py
@@ -8,7 +8,9 @@ class TicketTest(TestCase):
         ticket = Ticket()
         ticket.subject = 'Welcome to Pronto Service'
         ticket.requester = 'client@hisotech.com'
+        ticket.requester_id = '1095195473'
         ticket.assignee = 'kan@prontomarketing.com'
+        ticket.assignee_id = '1095195243'
         ticket.ticket_type = 'question'
         ticket.priority = 'urgent'
         ticket.tags = 'welcome'
@@ -18,7 +20,9 @@ class TicketTest(TestCase):
 
         self.assertEqual(ticket.subject, 'Welcome to Pronto Service')
         self.assertEqual(ticket.requester, 'client@hisotech.com')
+        self.assertEqual(ticket.requester_id, '1095195473')
         self.assertEqual(ticket.assignee, 'kan@prontomarketing.com')
+        self.assertEqual(ticket.assignee_id, '1095195243')
         self.assertEqual(ticket.ticket_type, 'question')
         self.assertEqual(ticket.priority, 'urgent')
         self.assertEqual(ticket.tags, 'welcome')
