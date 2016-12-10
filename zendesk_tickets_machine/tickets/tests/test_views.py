c6afabe2a19e041c5410611857b9af77f1b47951
zkan <kan@prontomarketing.com>
2016-12-10 15:36:54 +0700

    Render ticket form on page

--- zendesk_tickets_machine/tickets/tests/test_views.py
+++ zendesk_tickets_machine/tickets/tests/test_views.py
@@ -6,3 +6,42 @@ class TicketViewTest(TestCase):
     def test_ticket_view_should_be_accessiable(self):
         response = self.client.get(reverse('tickets'))
         self.assertEqual(response.status_code, 200)
+
+    def test_ticket_view_should_render_ticket_form(self):
+        response = self.client.get(reverse('tickets'))
+
+        expected = '<form method="post">'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = "<input type='hidden' name='csrfmiddlewaretoken'"
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_subject" maxlength="300" name="subject" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_comment" maxlength="500" name="comment" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_requester" maxlength="100" name="requester" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_requester_id" maxlength="50" name="requester_id" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_assignee" maxlength="100" name="assignee" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_assignee_id" maxlength="50" name="assignee_id" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_ticket_type" maxlength="50" name="ticket_type" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_priority" maxlength="50" name="priority" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input id="id_tags" maxlength="300" name="tags" type="text" required />'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input type="submit">'
+        self.assertContains(response, expected, status_code=200)
--- zendesk_tickets_machine/tickets/views.py
+++ zendesk_tickets_machine/tickets/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .forms import TicketForm
+
 
 class TicketView(TemplateView):
     template_name = 'tickets.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        form = TicketForm()
+        return render(
+            request,
+            self.template_name,
+            {
+                'form': form
+            }
+        )
