5cd5e62f2ca827544d2aff4ea4a7f9d53f6c1df1
zkan <kan@prontomarketing.com>
2016-12-14 07:23:24 +0700

    Create agent group model with name and Zendesk group ID

--- zendesk_tickets_machine/agent_groups/models.py
+++ zendesk_tickets_machine/agent_groups/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class AgentGroup(models.Model):
+    name = models.CharField(max_length=300)
+    zendesk_group_id = models.CharField(max_length=100)
---
+++ zendesk_tickets_machine/agent_groups/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from ..models import AgentGroup
+
+
+class AgentGroupTest(TestCase):
+    def test_save_agent_group(self):
+        agent_group = AgentGroup()
+        agent_group.name = 'Development'
+        agent_group.zendesk_group_id = '25050306'
+        agent_group.save()
+
+        agent_group = AgentGroup.objects.last()
+
+        self.assertEqual(agent_group.name, 'Development')
+        self.assertEqual(agent_group.zendesk_group_id, '25050306')
