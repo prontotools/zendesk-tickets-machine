d9e3db162438e212b3902b54fd1b8e5a8742eaf4
nattyait <nattanicha@gmail.com>
2016-12-20 16:02:48 +0700

    Home url should show list of tickets :baby

--- zendesk_tickets_machine/zendesk_tickets_machine/urls.py
+++ zendesk_tickets_machine/zendesk_tickets_machine/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^tickets/', include('tickets.urls')),
     url(r'^zendesk/', include('zendesk.urls')),
+    url(r'^', include('tickets.urls')),
 ]
