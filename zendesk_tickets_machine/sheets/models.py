a669e884efaf22400b245eed8d7d292473965b1f
nattyait <nattanicha@gmail.com>
2016-12-30 18:14:43 +0700

    Alter slug field to non-editable and autoganarated by slugify :baby:

--- zendesk_tickets_machine/sheets/models.py
+++ zendesk_tickets_machine/sheets/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Sheet(models.Model):
     name = models.CharField(max_length=300)
-    slug = models.SlugField(max_length=300)
+    slug = models.SlugField(max_length=300, null=True, blank=True, editable=False, unique=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Sheet, self).save(*args, **kwargs)
