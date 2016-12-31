0ce93b27490159d4ff6536bee58c596987f117a8
zkan <kan@prontomarketing.com>
2016-12-31 20:10:06 +0700

    Use base template :bear:

--- zendesk_tickets_machine/zendesk_tickets_machine/settings.py
+++ zendesk_tickets_machine/zendesk_tickets_machine/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'zendesk_tickets_machine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
