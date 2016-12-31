540ce973513a9dc0dd54c39c8d15bdc38b3b42fe
zkan <kan@prontomarketing.com>
2016-12-31 16:32:48 +0700

    Move zendesk tickets create view to board :bear:

--- zendesk_tickets_machine/boards/urls.py
+++ zendesk_tickets_machine/boards/urls.py
@@ -4,11 +4,14 @@ from .views import (
     BoardView,
     BoardResetView,
     BoardSingleView,
+    BoardZendeskTicketsCreateView,
 )
 
 
 urlpatterns = [
     url(r'^$', BoardView.as_view(), name='boards'),
+    url(r'^boardtickets/$',
+        BoardZendeskTicketsCreateView.as_view(), name='board_tickets_create'),
     url(r'^(?P<slug>[\w-]+)/$',
         BoardSingleView.as_view(), name='board_single'),
     url(r'^(?P<slug>[\w-]+)/reset/$',
--- zendesk_tickets_machine/zendesk/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .views import ZendeskTicketsCreateView
-
-
-urlpatterns = [
-    url(r'^tickets/$', ZendeskTicketsCreateView.as_view(),
-        name='zendesk_tickets_create'),
-]
