23116d6131f1be85d8091f956a1860c47bf19a6b
zkan <kan@prontomarketing.com>
2017-01-02 19:56:21 +0700

    Use Nginx to serve static files and set up Docker compose :bear:

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd $APPLICATION_ROOT\zendesk_tickets_machine/
 #python manage.py migrate --settings=zendesk_tickets_machine.settings
-#python manage.py collectstatic --noinput --settings=zendesk_tickets_machine.settings
+python manage.py collectstatic --noinput --settings=zendesk_tickets_machine.settings
 uwsgi --ini $APPLICATION_ROOT\ztm.ini
