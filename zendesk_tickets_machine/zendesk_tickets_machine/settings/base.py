0f5566663a9a238d9b96b49c18ec8e5127106f1d
zkan <kan@prontomarketing.com>
2017-01-16 10:04:16 +0700

    Set static root in base settings :bear:

--- zendesk_tickets_machine/zendesk_tickets_machine/settings/base.py
+++ zendesk_tickets_machine/zendesk_tickets_machine/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(SITE_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
