6c0d33d6301630cad9c1dd5430a61865d63d3c5a
nattyait <nattanicha@gmail.com>
2017-01-25 19:28:19 +0700

    Use reverse('login') and reverse('logout') instead :baby: #1

--- zendesk_tickets_machine/boards/tests/test_views.py
+++ zendesk_tickets_machine/boards/tests/test_views.py
@@ -1016,25 +1016,25 @@ class BoardZendeskTicketsCreateViewTest(TestCase):
 
 class LoginViewTest(TestCase):
     def test_need_login(self):
-        with self.settings(LOGIN_URL='/login/'):
+        with self.settings(LOGIN_URL=reverse('login')):
             response = self.client.get('/')
             self.assertRedirects(response, '/login/?next=/')
 
     def test_login_pass(self):
         User.objects.create_superuser('natty', 'natty@test', 'pass')
-        response = self.client.post('/login/', {
+        response = self.client.post(reverse('login'), {
             'username': 'natty', 'password': 'pass'})
         self.assertEqual(response.status_code, 302)
 
     def test_login_fail(self):
-        response = self.client.post('/login/', {
+        response = self.client.post(reverse('login'), {
             'username': 'john', 'password': 'smith'})
         self.assertEqual(response.status_code, 200)
 
     def test_logout(self):
         User.objects.create_superuser('natty', 'natty@test', 'pass')
-        self.client.post('/login/', {'username': 'natty', 'password': 'pass'})
-        self.client.post('/logout/')
-        with self.settings(LOGIN_URL='/login/'):
+        self.client.post(reverse('login'), {'username': 'natty', 'password': 'pass'})
+        self.client.post(reverse('logout'))
+        with self.settings(LOGIN_URL=reverse('login')):
             response = self.client.get('/')
             self.assertRedirects(response, '/login/?next=/')
