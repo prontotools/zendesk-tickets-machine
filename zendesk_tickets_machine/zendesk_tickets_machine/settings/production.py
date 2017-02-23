5dd39fee1d325ed2c38c0c1f6e93d27c668deec7
zkan <kan@prontomarketing.com>
2017-02-24 06:18:36 +0700

    Add Sentry and Raven configuration to production settings

--- zendesk_tickets_machine/zendesk_tickets_machine/settings/production.py
+++ zendesk_tickets_machine/zendesk_tickets_machine/settings/production.py
@@ -6,6 +6,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS = INSTALLED_APPS + [
+    'raven.contrib.django.raven_compat',
+]
+
+RAVEN_CONFIG = {
+    'dsn': os.environ.get('SENTRY_DSN')
+}
+
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
