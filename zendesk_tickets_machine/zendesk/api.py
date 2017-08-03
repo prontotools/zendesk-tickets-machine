b098529ce325172c8573d4a1b4fca7e53b72d68f
zkan <kan@prontomarketing.com>
2017-08-03 21:28:47 +0700

    Use new string interpolation (Python 3.6) :bear: #52

--- zendesk_tickets_machine/zendesk/api.py
+++ zendesk_tickets_machine/zendesk/api.py
@@ -21,9 +21,7 @@ class Ticket(object):
         return response.json()
 
     def create_comment(self, data, ticket_id):
-        url = self.zendesk_api_url + '/api/v2/tickets/{}.json'.format(
-            ticket_id
-        )
+        url = self.zendesk_api_url + f'/api/v2/tickets/{ticket_id}.json'
         response = requests.put(
             url,
             auth=(self.zendesk_api_user, self.zendesk_api_token),
